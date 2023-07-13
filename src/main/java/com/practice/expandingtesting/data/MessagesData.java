ae729427531e34de0e7a948a336bd3bde26ca100
Thaysetss <thayse.tss@gmail.com>
2023-07-13 00:08:27 -0300

    Adding the tests for login endpoint

--- src/main/java/com/practice/expandingtesting/data/MessagesData.java
+++ src/main/java/com/practice/expandingtesting/data/MessagesData.java
@@ -6,7 +6,9 @@ public enum MessagesData {
     USER_REGISTER_INVALID_EMAIL("A valid email address is required"),
     USER_REGISTER_NULL_NAME("User name must be between 4 and 30 characters"),
     USER_REGISTER_NULL_PASSWORD("Password must be between 6 and 30 characters"),
-    USER_LOGIN_SUCCESS("Login successful");
+    USER_LOGIN_SUCCESS("Login successful"),
+    USER_LOGIN_INCORRECT_EMAIL_PASSWORD("Incorrect email address or password"),
+    USER_LOGIN_PASSWORD_LESS_CHARACTERS("Password must be between 6 and 30 characters");
 
     public final String message;
 
--- src/test/java/expandingtesting/user/UserLoginTests.java
+++ src/test/java/expandingtesting/user/UserLoginTests.java
@@ -3,19 +3,18 @@ package expandingtesting.user;
 import com.practice.expandingtesting.client.users.UsersClient;
 import com.practice.expandingtesting.factory.UserFactory;
 import io.restassured.response.ValidatableResponse;
-import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.practice.expandingtesting.data.MessagesData.USER_LOGIN_SUCCESS;
-import static org.apache.http.HttpStatus.SC_OK;
+import static com.practice.expandingtesting.data.MessagesData.*;
+import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.Matchers.*;
 
 public class UserLoginTests {
 
     @Test
-    @DisplayName("Test add new user successfully with random data.")
-    void createNewRandomDataUser() {
+    @DisplayName("Test login with valid data from user created successfully.")
+    void userLoginValidUserCreatedSuccessfully() {
         var user = new UserFactory().generateRandomUser();
         new UsersClient().postRegisterNewRandomUser(user);
         ValidatableResponse response = new UsersClient().postLogin(user);
@@ -27,5 +26,52 @@ public class UserLoginTests {
                 .body("data.name", is(user.getName()))
                 .body("data.email", is(user.getEmail()))
                 .body("data.token", is(not(empty())));
+    }
+
+    @Test
+    @DisplayName("Test login with an invalid password, resulting in error 401.")
+    void userLoginInvalidPassword() {
+        var user = new UserFactory().generateRandomUser();
+        new UsersClient().postRegisterNewRandomUser(user);
+        user.setPassword("wrong_password");
+        ValidatableResponse response = new UsersClient().postLogin(user);
+        response.statusCode(SC_UNAUTHORIZED)
+                .body("success", is(false))
+                .body("message", is(USER_LOGIN_INCORRECT_EMAIL_PASSWORD.message));
+    }
+
+    @Test
+    @DisplayName("Test login with an email that was not created, resulting in error 401.")
+    void userLoginNonexistentEmail() {
+        var user = new UserFactory().generateRandomUser();
+        new UsersClient().postRegisterNewRandomUser(user);
+        user.setEmail("nonexistent@newemail.com");
+        ValidatableResponse response = new UsersClient().postLogin(user);
+        response.statusCode(SC_UNAUTHORIZED)
+                .body("success", is(false))
+                .body("message", is(USER_LOGIN_INCORRECT_EMAIL_PASSWORD.message));
+    }
+
+    @Test
+    @DisplayName("Test login with null email and result status code 400.")
+    void userLoginNullEmail() {
+        var user = new UserFactory().generateUserNullEmail();
+        new UsersClient().postRegisterNewRandomUser(user);
+        ValidatableResponse response = new UsersClient().postLogin(user);
+        response.statusCode(SC_BAD_REQUEST)
+                .body("success", is(false))
+                .body("message", is(USER_REGISTER_INVALID_EMAIL.message));
+    }
+
+    @Test
+    @DisplayName("Test login with a password with less than six characters, resulting in error 400.")
+    void userLoginLessPasswordCharacters() {
+        var user = new UserFactory().generateRandomUser();
+        new UsersClient().postRegisterNewRandomUser(user);
+        user.setPassword("123pa");
+        ValidatableResponse response = new UsersClient().postLogin(user);
+        response.statusCode(SC_BAD_REQUEST)
+                .body("success", is(false))
+                .body("message", is(USER_LOGIN_PASSWORD_LESS_CHARACTERS.message));
     }
 }
